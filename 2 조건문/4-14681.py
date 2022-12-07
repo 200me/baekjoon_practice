98c493345a0ee6de60c8f05be6512d575379ebdb
LEE JI HUN <dlwlgns0517@gmail.com>
2022-12-07 14:31:42 +0900

    20221207

--- 2 조건문/4-14681.py
+++ 2 조건문/4-14681.py
@@ -1,10 +1,24 @@
+# x = int(input())
+# y = int(input())
+# if x > 0 and y > 0:
+#     print("1")
+# elif x < 0 and y > 0:
+#     print("2")
+# elif x < 0 and y < 0:
+#     print("3")
+# elif x > 0 and y < 0:
+#     print("4")
+
 x = int(input())
 y = int(input())
-if x > 0 and y > 0:
-    print("1")
-elif x < 0 and y > 0:
-    print("2")
-elif x < 0 and y < 0:
-    print("3")
-elif x > 0 and y < 0:
-    print("4")
+q = 0
+sign = 1
+if y < 0:
+    q = 5
+    sign = -1
+
+if x < 0:
+    q += 2 * sign
+elif x > 0:
+    q += sign
+print(q)
---
+++ 3 반복문/2-10950.py
@@ -0,0 +1,5 @@
+test = int(input())
+
+for i in range(test):
+    num1, num2 = map(int, list(input().split()))
+    print(num1+num2)
