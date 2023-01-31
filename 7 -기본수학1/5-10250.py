4dbfc7b97b7d4eaf0f1fe6c87ccc140b5ada20ed
LEE JI HUN <dlwlgns0517@gmail.com>
2023-01-31 22:43:05 +0900

    Update 5-10250.py

--- 7 -기본수학1/5-10250.py
+++ 7 -기본수학1/5-10250.py
@@ -1,4 +1,8 @@
 T = int(input())
 
-for i in range(T):
-    H, W, N = map(int, list(input().split()))
+for i in range(T):  # 몇 번 돌릴건지
+    H, W, N = map(int, list(input().split()))  # 호텔 층 수, 각 층의 방 수, 몇 번째 손님
+    num = N // H + 1  # 방 번호(뒷자리)
+    floor = N % H  # 층 수 (앞자리)
+    if N % H == 0:
+        num = N // H
