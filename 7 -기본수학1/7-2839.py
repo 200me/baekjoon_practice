003398b44d4b97be2847ee8bbe47d9105dfa0f31
LEE JI HUN <dlwlgns0517@gmail.com>
2023-02-05 22:36:52 +0900

    Update 7-2839.py

--- 7 -기본수학1/7-2839.py
+++ 7 -기본수학1/7-2839.py
@@ -5,8 +5,10 @@ three = N // 3
 five1 = N % 5
 three1 = N % 3
 
-if five1 == 0:
-    dr += five
-    print(dr)
-elif not five1 == 0:
-    dr = N//5
+while N >= 0 :
+    if five1 == 0:
+        dr += five
+        print(dr)
+        break
+    elif not five1 == 0:
+        dr = N//5
