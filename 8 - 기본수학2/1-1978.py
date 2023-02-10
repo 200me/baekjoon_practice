cf1fbcbb09d0a6c12f0ff3ef195a211d6348e9fa
LEE JI HUN <dlwlgns0517@gmail.com>
2023-02-10 21:29:04 +0900

    Update 1-1978.py

--- 8 - 기본수학2/1-1978.py
+++ 8 - 기본수학2/1-1978.py
@@ -1,8 +1,15 @@
 N = int(input())
-num =  map(int, list(input().split()))
+num = map(int, list(input().split()))
 count = 0
 
 for i in num:
     error = 0
-    if i > 1 :
-        for
+    if (i == 1):
+        continue
+    for j in range(2, i + 1):
+        if (i % j == 0):
+            error += 1
+
+    if (error == 1):
+        count += 1
+print(count)
