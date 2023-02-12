bb40cebebdd6fffefbf6dd86959436d9f872d332
LEE JI HUN <dlwlgns0517@gmail.com>
2023-02-12 11:44:49 +0900

    20230212

--- 8 - 기본수학2/2-2581.py
+++ 8 - 기본수학2/2-2581.py
@@ -6,5 +6,15 @@ num_list = []
 for i in range(M, N + 1):
     ex = 0
     if i > 1:
-        for i in range(2, i):
-            error 
+        for j in range(2, i):
+            if i % j == 0:
+                ex += 1
+                break
+        if ex == 0:
+            num_list.append(i)
+if len(num_list) > 0:
+    print(sum(num_list))
+    print(min(num_list))
+
+else :
+    print(-1)
