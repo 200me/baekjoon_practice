33c9ead5441425082ea93e368c1e22507771b4b1
LEE JI HUN <dlwlgns0517@gmail.com>
2023-02-26 19:44:25 +0900

    20230226

--- 4 1차원배열/10-10810.py
+++ 4 1차원배열/10-10810.py
@@ -1,6 +1,9 @@
-N, M = map(int,input().split())
-ba = [0] * (N+1)
+N, M = map(int, input().split())
+ba = [0] * (N + 1)
 
 for x in range(M):
-    i, j, k = map(int,input().split())
-    for y in range()
+    i, j, k = map(int, input().split())
+    for y in range(i, j + 1, 1):
+        ba[y - 1] = k
+for x in range(N):
+    print(ba[x], '', end='')
