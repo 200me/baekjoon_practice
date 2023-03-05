1de3218e41887bacfe80acdf7952fdb7733e10ff
LEE JI HUN <dlwlgns0517@gmail.com>
2023-03-05 17:15:45 +0900

    Update 12-10811.py

--- 4 1차원배열/12-10811.py
+++ 4 1차원배열/12-10811.py
@@ -1,7 +1,7 @@
 N, M = map(int, input().split())
 data = [i for i in range(N + 1)]
 
-<<<<<<< HEAD
+
 for _ in range(M):
     i, j = map(int, input().split())
     value = []
@@ -16,9 +16,6 @@ for _ in range(M):
 
 for k in range(1, len(data)):
     print(data[k], end=' ')
-=======
-for _ in range(m) :
-  i, j = map(int, input().split())
-  value = []
-  for k in range(i, j + 1) :
->>>>>>> e6899936b4232275435a7010b35d57699bd267c3
+
+
+
