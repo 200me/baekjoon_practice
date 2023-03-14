3d35a4286353c708c9426da9b78e4e313e8637fa
LEE JI HUN <dlwlgns0517@gmail.com>
2023-03-14 23:06:57 +0900

    20230314

--- 6 문자열/5-1157.py
+++ 6 문자열/5-1157.py
@@ -1,12 +1,12 @@
-abc = input().upper()
+abc = input().lower()
 abc_list = list(set(abc))  # 중복 제거
 num = []
 
-for i in abc:
-    count = abc.count  # 개수 세는 함수
-    num.append(count(i))
+for i in abc_list:
+    count = abc.count(i)  # 개수 세는 함수
+    num.append(count)
 
-if num.count(max(num)) > 1:
+if num.count(max(num)) >= 2:
     print("?")
 else:
-    print(abc_list[(num.index(max(num)))])
+    print(abc_list[(num.index(max(num)))].upper())
