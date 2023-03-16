6994b923c2075af59a761ebce87f7b2ca9674ef4
LEE JI HUN <dlwlgns0517@gmail.com>
2023-03-16 23:02:11 +0900

    Update 3-10812.py

--- 심화1/3-10812.py
+++ 심화1/3-10812.py
@@ -3,4 +3,6 @@ N, M = map(int, input().split())
 arr = [i+1 for i in range(N)]
 for _ in range(M):
     i,j,k = map(int,input().split())
-    arr = arr[]
+    arr = arr[:i-1]+arr[k-1:j]+arr[i-1:k-1]+arr[j:]
+for b in arr:
+    print(b,end=' ')
