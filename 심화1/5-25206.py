f23ccdd6e855b095711ef1f074111933eecbfc71
LEE JI HUN <dlwlgns0517@gmail.com>
2023-03-21 23:27:35 +0900

    Update 5-25206.py

--- 심화1/5-25206.py
+++ 심화1/5-25206.py
@@ -1,4 +1,4 @@
-rating = ['A+', 'A0', 'B+', 'B0', 'C+', ' C0', 'D+', 'D0', 'F']
+rating = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
 grade = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
 
 total = 0
@@ -9,3 +9,5 @@ for _ in range(20):
     if g != 'P':
         total += p
         result += p * grade[rating.index(g)]
+
+print('%.6f' % (result/total))
