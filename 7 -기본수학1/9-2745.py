959680a625e1e5946443b4ef6780f68c1c5d1941
LEE JI HUN <dlwlgns0517@gmail.com>
2023-04-03 12:43:15 +0900

    Update 9-2745.py

--- 7 -기본수학1/9-2745.py
+++ 7 -기본수학1/9-2745.py
@@ -5,3 +5,9 @@ B = int(B)
 number = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 result  = 0
+
+for i in range(len(N)-1,-1,-1):
+    sum = number.index(N[i]) * (B**i)
+    result += sum
+
+print(result)
