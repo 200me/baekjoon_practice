86ebc0cfee9c13f2b3fedd772908f87cee36157b
LEE JI HUN <dlwlgns0517@gmail.com>
2023-04-09 22:20:32 +0900

    20230409

--- 7 -기본수학1/11-2720.py
+++ 7 -기본수학1/11-2720.py
@@ -1,13 +1,22 @@
-T = int(input())
+# T = int(input())
+#
+# money = [25, 10, 5, 1]
+# count = [0] * 4
+#
+# for i in range(T):
+#     C = int(input())
+#
+#     for j in range(len(money)):
+#         count[j] = C // money[j]
+#         C = C % money[j]
+#
+#         print(' '.join(map(str, count)))
 
-money = [25, 10, 5, 1]
-count = [0] * 4
-
-for i in range(T):
+for _ in range(int(input())):
     C = int(input())
-
-    for j in range(len(money)):
-        count[j] = C // money[j]
-        C = C % money[j]
-
-        print(' '.join(map(str, count)))
+    NUM = [25, 10, 5, 1]
+    li = []
+    for n in NUM:
+        li.append(C // n)
+        C = C % n
+    print(*li)
