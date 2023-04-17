023fd952ee5632e9f27abc300b2669c21794ce23
LEE JI HUN <dlwlgns0517@gmail.com>
2023-04-17 18:35:41 +0900

    Update 2-2566.py

--- 11- 2차원배열/2-2566.py
+++ 11- 2차원배열/2-2566.py
@@ -1,13 +1,24 @@
-# list1 = [list(map(int, input().split())) for _ in range(9)]
-#
+list1 = [list(map(int, input().split())) for _ in range(9)]
+
+max_num = 0
+max_row, max_col = 0, 0
+for row in range(9):
+    for col in range(9):
+        if max_num <= list1[row][col]:
+            max_row = row + 1
+            max_col = col + 1
+            max_num = list1[row][col]
+
+print(max_num)
+print(max_row, max_col)
+
 # max_num = 0
-# max_row, max_col = 0, 0
-# for row in range(9):
-#     for col in range(9):
-#         if max_num <= list1[row][col]:
-#             max_row = row + 1
-#             max_col = col + 1
-#             max_num = list1[row][col]
 #
+# for i in range(9):
+#     row = list(map(int, input().split()))
+#     if max(row) > max_num:
+#         max_num = max(row)
+#         x = i + 1
+#         y = row.index(max_num) + 1
 # print(max_num)
-# print(max_row, max_col)
+# print(x,y)
