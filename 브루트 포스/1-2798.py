7e470925fe0be0729ef7cca1348133bd8ae38b2d
LEE JI HUN <dlwlgns0517@gmail.com>
2023-04-27 19:42:41 +0900

    Update 1-2798.py

--- 브루트 포스/1-2798.py
+++ 브루트 포스/1-2798.py
@@ -5,3 +5,9 @@ result = 0
 for i in range(N):
     for j in range(i+1, N):
         for x in range(j+1, N):
+            if list_1[i] + list_1[j] + list_1[x] > M:
+                continue
+            else:
+                result = max(result, list_1[i]+ list_1[j]+ list_1[x])
+
+print(result)
