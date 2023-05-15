32825fb0591b71587ce05a17258fb16eacdd3f18
LEE JI HUN <dlwlgns0517@gmail.com>
2023-05-15 21:38:30 +0900

    Update 1-10815.py

--- 집합과 맵/1-10815.py
+++ 집합과 맵/1-10815.py
@@ -10,4 +10,19 @@
 #     else:
 #         print(0, end=' ')
 
+N = int(input())
+card = list(map(int, input().split()))
+M = int(input())
+other = list(map(int, input().split()))
 
+dic = []
+
+for o in other:
+    dic[o] = 0
+
+for c in card:
+    if c in dic:
+        dic[c] = 1
+
+for d in dic:
+    print(dic[d], end=' ')
