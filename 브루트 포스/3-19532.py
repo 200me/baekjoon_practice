ced282cdbc064ae4695c37b090585fefae9403c1
LEE JI HUN <dlwlgns0517@gmail.com>
2023-05-31 18:19:41 +0900

    20230531

--- 브루트 포스/3-19532.py
+++ 브루트 포스/3-19532.py
@@ -1,8 +1,12 @@
 A, B, C, D, E, F = map(int(input().split()))
 
+result = []
 for i in range(-999, 1000):
     for j in range(-999, 1000):
         if (A * i) + (B * j) == C and (D * i) + (E * j) == F:
             print(i, j)
             break
 #런타임 에러
+
+for i in range(2):
+    print(result[i], end='')
