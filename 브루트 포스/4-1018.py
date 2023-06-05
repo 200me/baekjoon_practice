c482834d28ae029e64bfce832fe0c91ce145aa10
LEE JI HUN <dlwlgns0517@gmail.com>
2023-06-05 21:16:54 +0900

    20230605

--- 브루트 포스/4-1018.py
+++ 브루트 포스/4-1018.py
@@ -1,25 +1,25 @@
 N, M = map(int, input().split())
-original = []
+ord = []
 count = []
 
 for _ in range(N):
-    original.append(input())
+    ord.append(input())
 
-for a in range(N-7):
-    for b in range(M-7):
+for a in range(N - 7):
+    for b in range(M - 7):
         index1 = 0
         index2 = 0
-        for i in range(a, a+8):
-            for j in range(b, b+8):
-                if (i+j) % 2 == 0:
-                    if original[i][j] != 'W':
+        for i in range(a, a + 8):
+            for j in range(b, b + 8):
+                if (i + j) % 2 == 0:
+                    if ord[i][j] != 'W':
                         index1 += 1
-                    if original[i][j] != 'B':
+                    if ord[i][j] != 'B':
                         index2 += 1
                 else:
-                    if original[i][j] != 'B':
+                    if ord[i][j] != 'B':
                         index1 += 1
-                    if original[i][j] != 'W':
+                    if ord[i][j] != 'W':
                         index2 += 1
         count.append(min(index1, index2))
 
