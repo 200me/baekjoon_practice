a4222b19811495dc77a1538870f78ddc2812af29
LEE JI HUN <dlwlgns0517@gmail.com>
2023-06-18 08:32:01 +0900

    2

---
+++ 집합과 맵/2-14425.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+set_1 = []
+set_2 = []
+for i in range(N + M):
+    sentence = input()
+    if i < N:
+        set_1.append(sentence)
+    else:
+        set_2.append(sentence)
+
+set_1 = set(set_1)
+
+cnt = 0
+for i in set_2:
+    if i in set_1:
+        cnt += 1
+print(cnt)
